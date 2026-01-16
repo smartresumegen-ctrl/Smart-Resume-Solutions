7ec20819dfc54d31d8e91e2574a7fcf6fbed17b6
smartresumegen-ctrl <smartresumegen@gmail.com>
2026-01-15 23:01:10 -0500

    Add Google Analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
 import '../styles/globals.css'
+import Script from 'next/script'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      {/* Google Analytics */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-Z81H775V5V"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-G-Z81H775V5V');
+        `}
+      </Script>
+      
+      <Component {...pageProps} />
+    </>
+  )
 }
+
+export default MyApp
